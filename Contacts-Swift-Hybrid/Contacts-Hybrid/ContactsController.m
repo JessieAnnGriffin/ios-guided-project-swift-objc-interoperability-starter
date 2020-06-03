bd70b5085c5dd19cd044dd786a35a57730c881c1
Jessie <jessie.griffin@icloud.com>
2020-06-02 21:45:31 -0400

    Added model controller in ObjC and Bridging Header

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/ContactsController.m
@@ -0,0 +1,13 @@
+//
+//  ContactsController.m
+//  Contacts-Hybrid
+//
+//  Created by Jessie Ann Griffin on 6/2/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "ContactsController.h"
+
+@implementation ContactsController
+
+@end
